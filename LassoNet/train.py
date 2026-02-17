2a602c6ee5bd06a340cc0b5fd298c8b40ceb369b
Jhonatan Ramos Felix (jrfelix@uliege.be) <jrfelix@lyra-l01.lyra.h6n.ulb.ac.be>
2026-02-17 13:51:43 +0100

    Finished train.py file

--- LassoNet/train.py
+++ LassoNet/train.py
@@ -1,32 +1,24 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from pandas_plink import read_plink
-from sklearn.preprocessing import StandardScaler, scale
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
-from LassoNet.constants import pheno_path, gen_path
-import torch 
-import pickle
 import argparse
-from datetime import datetime
-from lassonet import LassoNetClassifier, plot_cv, plot_path
-from lassonet.interfaces import LassoNetClassifierCV
-from lassonet import LassoNetRegressor
 import logging
+import pickle
+
+import numpy as np
+import pandas as pd
+import torch
+from constants import gen_path, pheno_path
+from lassonet import LassoNetRegressor
+from pandas_plink import read_plink
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 def setup_logger(log_filename):
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s | %(levelname)s | %(message)s",
-        handlers=[
-            logging.FileHandler(log_filename),
-            logging.StreamHandler()
-        ],
-        )
+        handlers=[logging.FileHandler(log_filename), logging.StreamHandler()],
+    )
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -37,27 +29,22 @@ def parse_args():
         "-n",
         "--n-samples",
         type=int,
-        default=20*(10**3),
-        help="Number of samples to use from the dataset (default:20k)"
-
+        default=20 * (10**3),
+        help="Number of samples to use from the dataset (default:20k)",
     )
     parser.add_argument(
         "-t",
         "--trait",
         type=int,
         required=True,
-        help="Trait index to train on:" \
-        " 2. " \
-        " 3." \
-        " 4." \
-        " 5."
+        help="Trait index to train on: 2.  3. 4. 5.",
     )
     return parser.parse_args()
 
 
 def main():
     # Loading arguments and logger
-    args = parse_args()    
+    args = parse_args()
     log_filename = f"train_trait{args.trait}_n{args.n_samples}.log"
 
     setup_logger(log_filename)
@@ -67,28 +54,33 @@ def main():
 
     # Loading genomical and phenotypical data
 
-    pheno_df = pd.read_csv(pheno_path, delimiter ='\t', header = None)
+    pheno_df = pd.read_csv(pheno_path, delimiter="\t", header=None)
 
     pheno_df = pheno_df.drop(columns=[0])
     pheno_df = pheno_df.set_index(1)
-    pheno_df.index.name == 'iid'
+    pheno_df.index.name == "iid"
 
     bim, fam, bed = read_plink(gen_path)
 
-    fam = fam.set_index('iid')
+    fam = fam.set_index("iid")
 
     # Separating data to training
-    y_with_labels = pheno_df[args.trait][~pheno_df[args.trait].isna()]
     y = pheno_df[args.trait][~pheno_df[args.trait].isna()].to_numpy()
 
     ## Creating a mask to identify the numbers of rows relative to each identification number
-    fam_pheno = fam['i'][~pheno_df[args.trait].isna()]
+    fam_pheno = fam["i"][~pheno_df[args.trait].isna()]
 
-    X_complete = bed.T[fam_pheno] # TODO: I can make a function here to match the shape of the matrix with the output
-    logging.info(f"The genotic data on the .bed matrix is on the shape:{X_complete.shape} \n")
-    
-    X = X_complete[:, :args.n_samples].compute()
-    logging.info(f'We managed to compute the genetic matrix, now the new shape is: {X.shape}')
+    X_complete = bed.T[
+        fam_pheno
+    ]  # TODO: I can make a function here to match the shape of the matrix with the output
+    logging.info(
+        f"The genotic data on the .bed matrix is on the shape:{X_complete.shape} \n"
+    )
+
+    X = X_complete[:, : args.n_samples].compute()
+    logging.info(
+        f"We managed to compute the genetic matrix, now the new shape is: {X.shape}"
+    )
     del X_complete
     del fam_pheno
     del bim
@@ -99,7 +91,50 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     del X
-    
+
+    scaler_X = StandardScaler()
+    X_train = scaler_X.fit_transform(X_train)
+    X_test = scaler_X.transform(X_test)
+
+    scaler_y = StandardScaler()
+    y_train = scaler_y.fit_transform(y_train.reshape(-1, 1)).ravel()
+    y_test = scaler_y.transform(y_test.reshape(-1, 1)).ravel()
+
+    X_train = X_train.astype(np.float32)
+    X_test = X_test.astype(np.float32)
+    y_train = y_train.astype(np.float32)
+    y_test = y_test.astype(np.float32)
+
+    optim_fn = lambda params: torch.optim.Adam(params, lr=1e-4)
+
+    model = LassoNetRegressor(
+        hidden_dims=(10, 10),
+        verbose=True,
+        patience=(100, 5),
+        optim=(optim_fn, optim_fn),
+        lambda_start=5,
+    )
+    path = model.path(X_train, y_train, return_state_dicts=True)
+
+    data_to_save = {
+        "path": path,
+        "X_train": X_train,
+        "X_test": X_test,
+        "y_train": y_train,
+        "y_test": y_test,
+    }
+
+    del X_train
+    del path
+    del X_test
+    del y_train
+    del y_test
+
+    with open(f"lassonet_{int(args.n_samples / 1000)}k_{args.trait}.pkl", "wb") as f:
+        pickle.dump(data_to_save, f)
+
     logging.info("Training finished")
 
 
+if __name__ == "__main__":
+    main()
