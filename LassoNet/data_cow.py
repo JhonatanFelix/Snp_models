e2116a85668cac505871cb57f970cd1ab0bd736a
jhonatan <jhonyrf@gmail.com>
2026-04-21 22:27:03 +0200

    preprocessing data

--- LassoNet/data_cow.py
+++ LassoNet/data_cow.py
@@ -1,1 +1,63 @@
-# load data
+import pandas as pd
+import numpy as np
+from pandas_plink import read_plink
+
+from sklearn.preprocessing import StandardScaler
+
+
+def load_cow_data(pheno_path_nan, pheno_path, gen_path, trait=2):
+    # Loading genomical and phenotypical data
+
+    def get_pheno_df(pheno_path):
+        pheno_df = pd.read_csv(pheno_path, delimiter="\t", header=None)
+        
+        pheno_df = pheno_df.drop(columns=[0])
+        pheno_df = pheno_df.set_index(1)
+        pheno_df.index.name == "iid"
+        return pheno_df
+
+    pheno_df_nan = get_pheno_df(pheno_path_nan)
+    pheno_df = get_pheno_df(pheno_path)
+
+    bim, fam, bed = read_plink(gen_path)
+
+    fam = fam.set_index("iid")
+
+    # Separating data to training
+    y_train = pheno_df[trait][~pheno_df_nan[trait].isna()].to_numpy()
+    y_test = pheno_df[trait][pheno_df_nan[trait].isna()].to_numpy()
+    y_test = y_test[~np.isnan(y_test)]
+
+    pheno_df["i"] = fam["i"]
+
+    ## Creating a mask to identify the numb~ers of rows relative to each identification number
+    fam_pheno = fam["i"][~pheno_df_nan[trait].isna()]
+    fam_pheno_test = pheno_df[pheno_df_nan[trait].isna()]
+    fam_pheno_test = fam_pheno_test["i"][~fam_pheno_test[trait].isna()]
+
+
+    X_train = bed.T[
+        fam_pheno
+    ]
+
+    X_test = bed.T[
+        fam_pheno_test
+    ]
+
+    del pheno_df_nan
+    del fam_pheno
+    del fam_pheno_test
+    del bim
+    del fam
+    del pheno_df
+    
+    scaler_X  = StandardScaler()
+    X_train = scaler_X.fit_transform(X_train)
+    X_test = scaler_X.transform(X_test)
+
+    scaler_y = StandardScaler()
+    y_train = scaler_y.fit_transform(y_train.reshape(-1,1)).reshape(-1)
+    y_test = scaler_y.transform(y_test.reshape(-1,1)).reshape(-1)
+
+    ## Spliting the dataset
+    return X_train, y_train, X_test, y_test
--- LassoNet/entry.py
+++ LassoNet/entry.py
@@ -1,11 +1,13 @@
 bo_config_file = 'param_space.yaml'
-ds_file = '....csv'
+pheno_path_nan = './ML/pheno_2023bbb_0twins_6traits_mask'
+pheno_path = './ML/pheno_20000bbb_6traits'
+gen_path = './ML/BBB2023_MD'
 
-from data import load_cow_csv_ds
+from data_cow import load_cow_data
 from bo import bo_search
 
 def main():
-    ds_tuple = load_cow_csv_ds(ds_file, testing_set_ratio=0.1)
+    ds_tuple = load_cow_data(pheno_path_nan, pheno_path, gen_path, trait=2)
     bo_search(ds_tuple, bo_config_file)
 
 if __name__ == "__main__":
